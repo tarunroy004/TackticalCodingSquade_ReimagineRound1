5488de9aed72778541c0180f4db1d7d48b1e9930
tarunroy004 <tarunroy181203@gmail.com>
2024-06-17 21:12:26 +0530

    Landig Page Ready

--- script.js
+++ script.js
@@ -16,11 +16,20 @@ Shery.makeMagnet("#magnet", {
 let menu = 0;
 document.querySelector("#btn").addEventListener("click", function() {
     if(menu == 1) {
+        gsap.to("body", {
+            overflowY: "auto"
+        })
         gsap.to("#nav", {
+            backgroundColor: "transparent",
+            backdropFilter: "none",
             width : "100%",
-            boxShadow: "0 0 0",
             duration: 0.6,
             ease: "power2"
+        })
+        gsap.to("#nav2", {
+            opacity: 1,
+            duration: 0.2,
+            ease: "power1"
         })
         gsap.to(".bar", {
             rotate: "0deg",
@@ -43,12 +52,20 @@ document.querySelector("#btn").addEventListener("click", function() {
         })
         menu = 0;
     } else {
+        gsap.to("body", {
+            overflowY: "hidden"
+        })
         gsap.to("#nav", {
+            backgroundColor: "rgba(37, 37, 37, 0.26)",
+            backdropFilter: "blur(10px)",
             width : "50vw",
-            height: "98vh",
-            boxShadow: "0 0 50px #ffffff67",
             duration: 0.5,
             ease: "power2"
+        })
+        gsap.to("#nav2", {
+            opacity: 0,
+            duration: 0.2,
+            ease: "power1"
         })
         gsap.to(".bar", {
             rotate: "-180deg",
@@ -74,11 +91,20 @@ document.querySelector("#btn").addEventListener("click", function() {
 })
 document.querySelector("#main").addEventListener("click", function() {
     if(menu == 1) {
+        gsap.to("body", {
+            overflowY: "auto"
+        })
         gsap.to("#nav", {
+            backgroundColor: "transparent",
+            backdropFilter: "none",
             width : "100%",
-            boxShadow: "0 0 0",
             duration: 0.6,
             ease: "power2"
+        })
+        gsap.to("#nav2", {
+            opacity: 1,
+            duration: 0.2,
+            ease: "power1"
         })
         gsap.to(".bar", {
             rotate: "0deg",
@@ -189,26 +215,20 @@ document.querySelector(".option4").addEventListener("mouseleave", function() {
     })
 })
 
+//Nav 1
+
 //Nav 2
-gsap.to("#nav2", {
-    bottom: "5%",
-    opacity: 1,
-    ease: "power2",
-    scrollTrigger : {
-        trigger: "#page2",
-        scroller: "body",
-        start: "top 50%",
-    }
-})
-gsap.to("nav", {
-    left: "110%",
+gsap.from("#nav2", {
+    bottom: "-10%",
     opacity: 0,
-    duration: "1.5",
     ease: "power2",
     scrollTrigger : {
         trigger: "#page2",
         scroller: "body",
-        start: "top 50%",
+        markers: false,
+        start: "top 90%",
+        end: "top 80%",
+        scrub: 1
     }
 })
 
@@ -261,7 +281,7 @@ document.querySelector("#nav2").addEventListener("click", () => {
     gsap.to("#nav2 #items", {
         opacity: 1,
         zIndex: "1000",
-        top: "-213px",
+        top: "-240px",
         duration: "0.5",
         ease: "power2"
     })
@@ -282,6 +302,189 @@ document.querySelector("#nav2 #items").addEventListener("mouseleave", ()=> {
     })
 })
 //PAge 2 
+gsap.from("#page2 #cards .img1 img", {
+    scale: 0.5,
+    top: "100%",
+    left: "56.5%",
+    rotate: "90deg",
+    scrollTrigger: {
+        trigger: "#page2",
+        scroller: "body",
+        markers: false,
+        start: "top 50%",
+        end: "top 25%",
+        scrub: 2,
+    }
+})
+gsap.from("#page2 #cards .img2 img", {
+    scale: 0.5,
+    top: "100%",
+    left: "56.5%",
+    rotate: "-90deg",
+    scrollTrigger: {
+        trigger: "#page2",
+        scroller: "body",
+        markers: false,
+        start: "top 40%",
+        end: "top 15%",
+        scrub: 2,
+    }
+})
+gsap.from("#page2 #cards .img3 img", {
+    scale: 0.5,
+    top: "100%",
+    left: "56.5%",
+    rotate: "90deg",
+    scrollTrigger: {
+        trigger: "#page2 #text",
+        scroller: "body",
+        markers: false,
+        start: "top 20%",
+        end: "top 5%",
+        scrub: 2,
+    }
+})
+gsap.from("#page2 #cards .img4 img", {
+    scale: 0.5,
+    top: "100%",
+    left: "56.5%",
+    rotate: "-90deg",
+    scrollTrigger: {
+        trigger: "#page2 #text",
+        scroller: "body",
+        markers: false,
+        start: "top 10%",
+        end: "top 0%",
+        scrub: 2,
+    }
+})
 
 
 
+// Page 3 
+// Text
+gsap.from("#page3 .heading",{
+    opacity: 0,
+    scrollTrigger: {
+        trigger: "#page3 .heading",
+        scroller: "body",
+        markers: false,
+        start: "top 70%",
+        stagger: "0.5"
+    }
+})
+
+
+// Page 4 
+// card1 to 5
+gsap.from("#page4 .card1", {
+    scale: 0.5,
+    top: "35%",
+    left: "56.5%",
+    rotate: "0deg",
+    scrollTrigger: {
+        trigger: "#page4 .card1",
+        scroller: "body",
+        markers: false,
+        start: "top 60%",
+        end: "top 25%",
+        scrub: 2,
+    }
+})
+gsap.from("#page4 .card2", {
+    scale: 0.5,
+    top: "36.5%",
+    left: "54%",
+    rotate: "0deg",
+    scrollTrigger: {
+        trigger: "#page4 .card2",
+        scroller: "body",
+        markers: false,
+        start: "top 60%",
+        end: "top 25%",
+        scrub: 2,
+    }
+})
+gsap.from("#page4 .card3", {
+    scale: 0.5,
+    top: "40%",
+    left: "50%",
+    rotate: "0deg",
+    scrollTrigger: {
+        trigger: "#page4 .card3",
+        scroller: "body",
+        markers: false,
+        start: "top 60%",
+        end: "top 25%",
+        scrub: 2,
+    }
+})
+gsap.from("#page4 .card4", {
+    scale: 0.5,
+    top: "41%",
+    left: "50%",
+    rotate: "0deg",
+    scrollTrigger: {
+        trigger: "#page4 .card4",
+        scroller: "body",
+        markers: false,
+        start: "top 60%",
+        end: "top 25%",
+        scrub: 2,
+    }
+})
+gsap.from("#page4 .card5", {
+    scale: 0.5,
+    top: "35%",
+    left: "59%",
+    rotate: "0deg",
+    scrollTrigger: {
+        trigger: "#page4 .card5",
+        scroller: "body",
+        markers: false,
+        start: "top 60%",
+        end: "top 25%",
+        scrub: 2,
+    }
+})
+
+
+
+// Time Reach
+window.addEventListener('load', function () {
+    function addZ(num) {
+      return num < 10 ? `0${num}` : num;
+    }
+  
+    let today = new Date();
+    let month = today.getMonth() + 1;
+    let year = today.getFullYear();
+    let date = today.getDate();
+    let hours = addZ(today.getHours());
+    let minutes = addZ(today.getMinutes());
+    let seconds = addZ(today.getSeconds());
+  
+    let current_time = `${hours}:${minutes}:${seconds}`;
+    time.innerHTML = current_time;
+  
+  })
+  let time = document.querySelector(".time");
+  let timeBtn = document.querySelector(".clickTime");
+  
+  window.addEventListener("mousemove", function () {
+    function addZ(num) {
+      return num < 10 ? `0${num}` : num;
+    }
+  
+    let today = new Date();
+    let month = today.getMonth() + 1;
+    let year = today.getFullYear();
+    let date = today.getDate();
+    let hours = addZ(today.getHours());
+    let minutes = addZ(today.getMinutes());
+    let seconds = addZ(today.getSeconds());
+  
+    let current_time = `${hours}:${minutes}:${seconds}`;
+    time.innerHTML = current_time;
+  
+  })
